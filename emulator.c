243ead612f4424222dc646cc790af692efeae33b
Harry Beadle <harry.beadle@gmail.com>
2017-02-10 00:15:19 +0000

    Inital

---
+++ emulator.c
@@ -0,0 +1,22 @@
+/*
+
+Harry Beadle
+hb11g15@soton.ac.uk
+
+Date Created: 10/02/2017
+
+ELEC2204 Computer Engineering
+Coursework: Computer Emulation
+
+*/
+
+#include "opcodes.h"
+
+#define MEMORY_SIZE 1024
+
+uint8_t memory[MEMORY_SIZE];
+
+int main(void)
+{
+	// TODO
+}
